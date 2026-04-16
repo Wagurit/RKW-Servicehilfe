239be4c4486bd7b8ef048cd2f5378d71e30c8697
Wagurit <salaquarda@wagurit.de>
2026-04-16 17:42:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rkw-v1';
+const CACHE = 'rkw-v2';
 const ASSETS = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
